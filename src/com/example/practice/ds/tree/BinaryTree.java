3a31da9aafd72eadae5903c0ce6a542cc6642bb9
Balraj <balraajbrar@gmail.com>
2019-07-14 23:03:58 +0530

    Solved problem of Building a complete binary tree from a given input and the level order printing procedure.

--- src/com/example/practice/ds/tree/BinaryTree.java
+++ src/com/example/practice/ds/tree/BinaryTree.java
@@ -1,8 +1,10 @@
 package com.example.practice.ds.tree;
 
 
+import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Queue;
 
 /**
  * A binary tree implementation in java
@@ -30,13 +32,65 @@ public class BinaryTree<T> {
         inorder(sequence, node.rightChild);
     }
 
+    /**
+     * Factory method to build an almost complete binary tree from the given input.
+     * @param elements list of elements to be inserted in level order in the tree
+     * @param <E> type of elements
+     * @return a binary tree with all levels except possibly the last one full
+     */
+    public static <E> BinaryTree<E> buildAlmostCompleteBinaryTree(List<E> elements){
+        BinaryTree<E> bt = new BinaryTree<>(elements.get(0));
+        bt.root = addChildren(bt.root, elements, 0);
+        return bt;
+    }
+
+    /**
+     * Helper method for buildAlmostCompleteBinaryTree
+     * @param root
+     * @param elements
+     * @param currentIndex
+     * @param <E>
+     * @return
+     */
+    private static <E> Node<E> addChildren(Node<E> root, List<E> elements, int currentIndex){
+        if(currentIndex<elements.size()){
+            root = new Node<>(elements.get(currentIndex),null,null,null);
+            root.leftChild = addChildren(root.leftChild, elements,currentIndex*2+1);
+            root.rightChild = addChildren(root.rightChild, elements, currentIndex*2+2);
+        }
+        return root;
+    }
+
+
+    /**
+     * This method returns a sequence of all the elements of tree in level order.
+     * @return
+     */
+    public List<T> getLevelOrder(){
+        List<T> sequence = new ArrayList<>();
+        List<Node<T>> nodes = new ArrayList<>();
+        nodes.add(this.root);
+        return helper(nodes, sequence);
+    }
+
+    private List<T> helper(List<Node<T>> nodes, List<T> sequence){
+        if(nodes.size()==0) return sequence;
+        Node<T> currentNode = nodes.remove(0);
+        if((currentNode!=null)){
+            sequence.add(currentNode.element);
+            nodes.add(currentNode.leftChild);
+            nodes.add(currentNode.rightChild);
+            helper(nodes,sequence);
+        }
+        return sequence;
+    }
 
     /**
      * This class represents a node in the binary tree
      * @param <T> type of element that would be stored in this node.
      *            all the nodes of a particular binary tree have same type.
      */
-    private class Node<T>{
+    private static class Node<T>{
         private T element;
         private Node<T> parent;
         private Node<T> leftChild;
--- src/com/example/practice/ds/tree/Tester.java
+++ src/com/example/practice/ds/tree/Tester.java
@@ -1,12 +1,16 @@
 package com.example.practice.ds.tree;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Tester {
 
     public static void main(String[] args){
-        BinaryTree<Integer> bt = new BinaryTree<>(123);
-        List<Integer> inorder = bt.traverseInorder();
-        System.out.print(inorder);
+        BinaryTree<Integer> bt = BinaryTree.buildAlmostCompleteBinaryTree(new ArrayList<>(Arrays.asList(1,2,3,4,5,6,7,8,9,10)));
+        List<Integer> seq = bt.getLevelOrder();
+        for(int i : seq){
+            System.out.print(" "+i);
+        }
     }
 }
