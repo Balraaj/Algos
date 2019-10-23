0a4711111a3116fe828eb7dbfa617852da991bea
Balraj <balraajbrar@gmail.com>
2019-10-23 22:20:51 +0530

    EPI: LinkedList: 8.1: Iterative implementation of merge procedure

--- src/com/epi/problems/linkedlists/ProblemSolver.java
+++ src/com/epi/problems/linkedlists/ProblemSolver.java
@@ -33,4 +33,30 @@ public class ProblemSolver {
             parent.next = first == null ? second : first;
         }
     }
+
+    /**
+     * EPI: LinkedList: 8.1: An iterative solution.
+     * @param l1
+     * @param l2
+     * @return
+     */
+    public static ListNode<Integer> mergeIterative(LinkedList<Integer> l1, LinkedList<Integer> l2){
+        ListNode<Integer> head = l1.HEAD.data<l2.HEAD.data ? l1.HEAD: l2.HEAD;
+        ListNode<Integer> parent  = head;
+        ListNode<Integer> runner1 = parent == l1.HEAD ? l2.HEAD : l1.HEAD;
+        ListNode<Integer> runner2 = parent.next;
+
+        /**
+         * Why runner1 cant be null ?
+         * because of the assignment to other runner;
+         */
+        while(runner1 != null && runner2 != null){
+            parent.next = runner1.data < runner2.data ? runner1 : runner2;
+            parent = parent.next;
+            runner1 = parent == runner1 ? runner2 : runner1;
+            runner2 = parent.next;
+        }
+        parent.next = runner1 == null ? runner2 : runner1;
+        return head;
+    }
 }
--- src/com/epi/problems/linkedlists/Runner.java
+++ src/com/epi/problems/linkedlists/Runner.java
@@ -3,9 +3,12 @@ package com.epi.problems.linkedlists;
 public class Runner {
 
     public static void main(String[] args){
-        LinkedList<Integer> first = LinkedList.getList(new Integer[]{1});
+        LinkedList<Integer> first = LinkedList.getList(new Integer[]{1,2,3,4,5,6,78,80});
         LinkedList<Integer> second = LinkedList.getList(new Integer[]{2,4,6,8,8,8,8,8,8,12,14,16,17});
-        LinkedList<Integer> result = ProblemSolver.merge(first, second);
-        LinkedList.print(result);
+        LinkedList.ListNode<Integer> result = ProblemSolver.mergeIterative(first, second);
+        while(result !=null){
+            System.out.print(result.data+", ");
+            result = result.next;
+        }
     }
 }
